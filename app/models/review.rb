79b1ae627fe2e7739bbb47dcfd8ef647d61c14ea
Dean Foulds <deanfoulds@gmail.com>
2016-08-17 15:43:50 +0100

    restaurant part of app finished

---
+++ app/models/review.rb
@@ -0,0 +1,3 @@
+class Review < ApplicationRecord
+  belonds_to :restaurant
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817110524) do
+ActiveRecord::Schema.define(version: 20160817114151) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160817110524) do
     t.string   "category"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "contnt"
+    t.integer  "ratings"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
